edb466ab90cd65468bebcf62d27259d507dcbddb
Tristan N <tristan071@gmail.com>
2021-03-31 22:28:40 +0200

    addddddddd

---
+++ Task1.1/Expenses.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1._1
+{
+public abstract class Expense
+    {
+        string name;
+        public abstract double Amount();
+        
+
+
+
+
+
+
+
+        
+    }
+
+    public class groceries : Expense
+
+    {
+        
+        public override double Amount()
+        {
+           
+           
+            
+
+            
+
+        }
+    }
+
+    public class utilities : Expense
+
+    {
+        public override void Amount()
+        {
+            Console.WriteLine("Please enter amount spent on utilities ( Water & Lights ) : ");
+            double value = double.Parse(Console.ReadLine());
+            //Console.WriteLine("You spend " + value + " on utilities a month");
+            
+        }
+    }
+
+
+    public class travelCosts : Expense
+
+    {
+        public override void Amount()
+        {
+            Console.WriteLine("Please enter amount spent on travel costs (Petrol etc.) : ");
+            double value = double.Parse(Console.ReadLine());
+            //Console.WriteLine("You spend " + value + " on travelCosts a month");
+        }
+    }
+
+    public class phoneBills : Expense
+
+    {
+        public override void Amount()
+        {
+
+            Console.WriteLine("Please enter amount spent on Cellular and telephonic calls:  ");
+            double value = double.Parse(Console.ReadLine());
+        }
+    }
+
+    public class otherExpense : Expense
+
+    {
+        public override void Amount()
+        {
+            Console.WriteLine("Please enter amount spent on any other catagories: ");
+            double value = double.Parse(Console.ReadLine());
+        }
+    }
+
+    public class rent : Expense
+
+    {
+        public override void Amount()
+        {
+            Console.WriteLine("Please enter amount spent on rent: ");
+            double value = double.Parse(Console.ReadLine());
+        }
+    }
+    //public class placeholder : Expense
+
+    //{
+    //    public override void Amount()
+    //    {
+
+
+    //    }
+    //}
+    public class homeLoan : Expense
+
+    {
+        public override void Amount()
+        {
+            Console.WriteLine("Please fill in the required information: ");
+
+        }
+    }
+
+}
---
+++ Task1.1/Program.cs
@@ -0,0 +1,102 @@
+﻿    using System;
+
+namespace Task1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            greetings();
+            monthlyIncome();
+
+
+
+
+
+            
+            //groceries amount = new groceries();
+            //amount.Amount();
+
+
+
+            //utilities amountUtil = new utilities();
+            //amountUtil.Amount();
+
+            //travelCosts amountTravel = new travelCosts();
+            //amountTravel.Amount();
+
+            //phoneBills amountPhone = new phoneBills();
+            //amountPhone.Amount();
+
+            //otherExpense amountOther = new otherExpense();
+            //amountOther.Amount();
+
+            
+         
+
+
+
+
+
+
+        }
+        static void greetings()
+        {
+            Console.WriteLine("----------------------------------------------------------------------------------------------------");
+            Console.WriteLine("Welcome to the Budget Planner Application!");
+            Console.WriteLine("----------------------------------------------------------------------------------------------------");
+            Console.WriteLine("");
+           
+        }
+
+
+        static void placeholer1()
+        {
+
+            Console.WriteLine("Please type 1 if renting a home or 2 if you are paying off a home loan? : ");
+
+            String answer = Console.ReadLine();
+
+
+            if (answer.Equals("1"))
+            {
+
+                Console.WriteLine("Please enter how much is spent on rent per month: ");
+
+
+            }
+            else if (answer.Equals("2"))
+            {
+
+                Console.WriteLine("Hello");
+
+            }
+
+            else 
+            {
+
+                Environment.Exit(0);
+
+            } 
+                
+        }
+
+static void  monthlyIncome()
+       {
+           Console.WriteLine("Please enter your total monthly income");
+            
+            
+        }
+
+        static void groceries()
+        {
+            Console.WriteLine("Please enter amount spent on groceries: ");
+
+
+        }
+
+
+
+    }
+}
