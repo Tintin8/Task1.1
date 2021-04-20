5df1b6b0a739fc57d07bd6f3a3e24bc4fb4c4991
Tristan N <tristan071@gmail.com>
2021-04-20 18:35:41 +0200

    Finished

--- Task1.1/Expenses.cs
+++ Task1.1/Expenses.cs
@@ -6,112 +6,251 @@ using System.Threading.Tasks;
 
 namespace Task1._1
 {
-    public abstract class Expense
+    public abstract class Expense                                                   // Original Abstract class from which all expense classes will inherit information from.
     {
 
 
+        public static double[] expenses = new double[9];                            // Array that stores in [0] stores the monthly income. and in [1]-[9] stores each relavent expense.
+        public abstract double Amount();                                            // Method that every expense inherits. Since its abstract it has no body and as its a double, all furthur classes will return a double value.
 
-        public abstract double Amount();
+        public static string enterAmountSpent()                                     // Method for each class to inherit that prompts the user to enter information
+        {
+            string enterAmount = "Please enter amount spent ";
+            return enterAmount;                                                                 
 
 
 
+        }
+        public static double newAmount()                                            // Method that  collects a value from the user input to be used in later calculations
+        {
+
+            Console.WriteLine("");
+            double value = double.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            return value;
 
 
+        }
 
+        
+
+    }
+    public class Tax : Expense                                                      // First class that inherits from the expense class above.
+
+    {
+
+        public void printLine()                                                     // Method that adds info to the next value reported by the user.
+        {
+
+            Console.WriteLine("");
+            Console.WriteLine(Tax.enterAmountSpent() + "on tax per month.");        // appended text to value
+            Console.WriteLine("");
+
+
+        }
+        public override double Amount()                                             // Overrides the method found in Expense and collects user input while storing it into the array in slot [1]
+        {
+
+            double cost = Tax.newAmount();
+            Expense.expenses[1] = cost;
+            return cost;                                                            // Since the amount() in expense is a double, the inherited class returns a value
+
+        }
 
 
 
     }
-
-    public class groceries : Expense
+    public class Groceries : Expense                                                // each class is the same from here until home loans as they all inherit the classes from Expense
 
     {
 
+        public void printLine()
+        {
+
+            Console.WriteLine("");
+            Console.WriteLine(Groceries.enterAmountSpent() + "on groceries per month.");
+            Console.WriteLine("");
+
+        }
         public override double Amount()
         {
-            Console.WriteLine("Please enter amount spent on gro ");
-            double value = double.Parse(Console.ReadLine());
-            Console.WriteLine("You spend " + value + " on groceries a month"); 
-            return value;
+
+            double cost = Groceries.newAmount();
+            Expense.expenses[2] = cost;
+            return cost;
+
+        }
+
+
+
+    }
+    public class Utilities : Expense
+
+    {
+        public void printLine()
+        {
+
+            Console.WriteLine("");
+            Console.Write(Utilities.enterAmountSpent() + "on utilites per month.");
+            Console.WriteLine("");
 
 
 
 
         }
+        public override double Amount()
+        {
+            double cost = Utilities.newAmount();
+            Expense.expenses[3] = cost;
+            return cost;
+
+        }
     }
-}
+    public class travelCosts : Expense
 
-    //public class utilities : Expense
+    {
+        public void printLine()
+        {
 
-//{
-//    public override void Amount()
-//    {
-//        Console.WriteLine("Please enter amount spent on utilities ( Water & Lights ) : ");
+            Console.WriteLine("");
+            Console.Write(travelCosts.enterAmountSpent() + "on travel costs per month.");
+            Console.WriteLine("");
+        }
+        public override double Amount()
+        {
+            double cost = travelCosts.newAmount();
+            Expense.expenses[4] = cost;
+            return cost;
 
-//        Console.WriteLine("You spend " + value + " on utilities a month");
+        }
+    }
+    public class phoneBills : Expense
 
-//    }
-//}
+    {
+        public void printLine()
+        {
 
+            Console.WriteLine("");
+            Console.Write(phoneBills.enterAmountSpent() + "on phone billing per month.");
+            Console.WriteLine("");
+        }
+        public override double Amount()
+        {
+            double cost = phoneBills.newAmount();
+            Expense.expenses[5] = cost;
+            return cost;
 
-//    public class travelCosts : Expense
+        }
+    }
+    public class otherExpense : Expense
 
-//    {
-//        public override void Amount()
-//        {
-//            Console.WriteLine("Please enter amount spent on travel costs (Petrol etc.) : ");
-//            double value = double.Parse(Console.ReadLine());
-//            //Console.WriteLine("You spend " + value + " on travelCosts a month");
-//        }
-//    }
+    {
+        public void printLine()
+        {
+            Console.WriteLine("");
+            Console.Write(otherExpense.enterAmountSpent() + "on other expenses per month.");
+            Console.WriteLine("");
+        }
+        public override double Amount()
+        {
+            double cost = otherExpense.newAmount();
+            Expense.expenses[6] = cost;
+            return cost;
 
-//    public class phoneBills : Expense
-
-//    {
-//        public override void Amount()
-//        {
-
-//            Console.WriteLine("Please enter amount spent on Cellular and telephonic calls:  ");
-//            double value = double.Parse(Console.ReadLine());
-//        }
-//    }
-
-//    public class otherExpense : Expense
-
-//    {
-//        public override void Amount()
-//        {
-//            Console.WriteLine("Please enter amount spent on any other catagories: ");
-//            double value = double.Parse(Console.ReadLine());
-//        }
-//    }
-
-//    public class rent : Expense
-
-//    {
-//        public override void Amount()
-//        {
-//            Console.WriteLine("Please enter amount spent on rent: ");
-//            double value = double.Parse(Console.ReadLine());
-//        }
-//    }
-//    //public class placeholder : Expense
-
-//    //{
-//    //    public override void Amount()
-//    //    {
+        }
 
 
-//    //    }
-//    //}
-//    public class homeLoan : Expense
+    }
+    public class Rent : Expense                                                                             // rent is a simple technique as its just added to the total expense as rent is a flat rate.
 
-//    {
-//        public override void Amount()
-//        {
-//            Console.WriteLine("Please fill in the required information: ");
+    {
+        public void printLine()
+        {
+            Console.WriteLine("");
+           
+            
+        }
+        public override double Amount()
+        {
+            double cost = Rent.newAmount();
+            Expense.expenses[7] = cost;
+            return cost;
 
-//        }
-//    }
+        }
 
-//}
+
+    }
+    public class homeLoan : Expense                                                      // homeloan has its own calculations that pertain to its specific situation 
+
+    {
+
+        public void printLine()                                                          // Just used to create a unified look in the presentation of the program
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+        }
+        public override double Amount()                                                   // the amount() method prompts users for input regarding the home loan and uses the equation to figure out the monthly payments to be made.
+        {
+
+
+
+
+
+            Console.WriteLine("Please enter Purchase price of property");
+            double propertyPrice = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter Total deposit");
+            double totalDeposit = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter Interest rate (percentage seperated by , if necessary)");
+            double interestRatePercentage = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Please enter Number of months to repay");
+            Console.WriteLine("Please check the number of months is correct(240-360)");
+
+            double repayMonths = double.Parse(Console.ReadLine());                                          // Collects the months value that will be used in the calculation
+            int months = Convert.ToInt32(repayMonths);
+       
+            double interestRateNumber = interestRatePercentage / 100;                                       // interest rate from user in decimal or int 12/2,3 etc divided by 100 to get a double to be used later.
+            double priceLessDeposit = propertyPrice - totalDeposit;                                         // price with deposit sabtracted in order to discover the amount left to be paid
+            double numberYears = repayMonths / 12;                                                         // number of years to pay out calculated by total months /12
+            double total = priceLessDeposit * (1 + interestRateNumber * numberYears);                       // total price of payment is price minus deposit times ( one + interest rate times number of years)
+      
+
+            double monthlyPayment = total / repayMonths;                                                    // Finds out monthly payment owed with provided information
+            Console.WriteLine("Your total monthly payments are: ");
+            Console.WriteLine("R" + monthlyPayment);
+            Expense.expenses[8] = monthlyPayment;                                                          // stores final amount per month into array slot 8
+
+             return monthlyPayment;                                                                        // returns final monthlypayment that will be added to other monthly payments in array
+                                
+
+
+           
+
+
+
+        }
+       
+
+    }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
--- Task1.1/Program.cs
+++ Task1.1/Program.cs
@@ -4,36 +4,38 @@ namespace Task1._1
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args)                                                                     // main method that will call each created function as well as line breaks to keep unity amoung output.
+        {                                                                                                   // no calculations will be kept in main as to make the app as scalable as possible
+
 
             greetings();
-           
+            Console.WriteLine("");
+            monthlyIncome();
+            tax();
+            Console.WriteLine("");
+            groceries();
+            Console.WriteLine("");
+            utilities();
+            Console.WriteLine("");
+            travelCosts();
+            Console.WriteLine("");
+            phoneBills();
+            Console.WriteLine("");
+            otherExpense();
+            Console.WriteLine("");
+            homePayment();
+            Console.WriteLine("");
+            approvalAlert();
+            Console.WriteLine("");
+            totalExpenses();
+            Console.WriteLine();
+            moneyLeft();
 
-
-
-
-
-            
-            groceries amount = new groceries();
-            amount.Amount();
-
-
-
-            //utilities amountUtil = new utilities();
-            //amountUtil.Amount();
-
-            //travelCosts amountTravel = new travelCosts();
-            //amountTravel.Amount();
-
-            //phoneBills amountPhone = new phoneBills();
-            //amountPhone.Amount();
-
-            //otherExpense amountOther = new otherExpense();
-            //amountOther.Amount();
-
-            
-         
+            ////Print array
+            //foreach (var item in Expense.expenses)
+            //{
+            //    Console.WriteLine(item.ToString());
+            //}
 
 
 
@@ -41,62 +43,150 @@ namespace Task1._1
 
 
         }
-        static void greetings()
+
+
+        static void greetings()                                                                                             // method to welcome the user to the application
         {
             Console.WriteLine("----------------------------------------------------------------------------------------------------");
             Console.WriteLine("Welcome to the Budget Planner Application!");
             Console.WriteLine("----------------------------------------------------------------------------------------------------");
             Console.WriteLine("");
-           
+
         }
 
 
-        static void placeholer1()
+        static void homePayment()                                                                   // method containing if statment that will determin via user input whether or not they will be renting , buying or not paying rent etc.
         {
 
-            Console.WriteLine("Please type 1 if renting a home or 2 if you are paying off a home loan? : ");
+            Console.WriteLine("Please type 1 if renting a home or 2 if you are paying off a home loan. ");
+            Console.WriteLine("If neither press 3 else press anything else to exit");
 
             String answer = Console.ReadLine();
+           
 
-
-            if (answer.Equals("1"))
+            if (answer.Equals("1"))                                                                 // if statement for determining living status costs
             {
 
                 Console.WriteLine("Please enter how much is spent on rent per month: ");
 
+                Rent amount = new Rent();
+                amount.printLine();
+                amount.Amount();
 
             }
             else if (answer.Equals("2"))
             {
 
-                Console.WriteLine("Hello");
+               
+
+                homeLoan amount = new homeLoan();
+
+                amount.Amount();
+
 
             }
 
+            else if (answer.Equals("3"))                                // if user chooses 3 the program continues with R0 paid for living expenses
+            {
+
+               //PLACEHOLDER
+               
+            }
             else 
             {
 
-                Environment.Exit(0);
+                System.Environment.Exit(0);
 
-            } 
                 
+            }
+
         }
 
-static void  monthlyIncome()
-       {
-           Console.WriteLine("Please enter your total monthly income");
-            
-            
-        }
-
-        static void groceries()
+        static double monthlyIncome()                                                           // monthly income is stored here and called. *note the only value calculation as it is not an expense
         {
-            Console.WriteLine("Please enter amount spent on groceries: ");
 
+            Console.WriteLine("Please enter your monthly income");
+            double answer = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Expense.expenses[0] = answer;                                                       // is stored at position [0] in the array as to help with unity
+            return answer;
+            
 
         }
+        static void tax()                                                                       // method to call tax and its functions
+        {
 
+            Tax amount = new Tax();
+            amount.printLine();
+            amount.Amount();
 
+        }
+        static void groceries()                                                                 // method to call groceries and its functions
+        {   
 
+            Groceries amount = new Groceries();                                                 // instatiating the object
+            amount.printLine();                                                                  // calling the method in the object
+            amount.Amount();
+           
+
+        }
+        static void utilities()                                                                  // method to call utilities and its functions
+        {
+
+            Utilities amount = new Utilities();
+            amount.printLine();
+            amount.Amount();
+            
+
+        }
+        static void travelCosts()                                                                  // method to call travel and its functions
+        {
+
+            travelCosts amount = new travelCosts();
+            amount.printLine();
+            amount.Amount();
+            
+        }
+        static void phoneBills()                                                                  // method to call cellular and its functions
+        {
+
+            phoneBills amount = new phoneBills();
+            amount.printLine();
+            amount.Amount();
+            
+        }
+        static void otherExpense()                                                                  // method to call otherexpenses and its functions
+        {
+
+            otherExpense amount = new otherExpense();
+            amount.printLine();
+            amount.Amount();
+            
+        }
+        static void approvalAlert()                                                                 // method that checks whether or not the monthly homeloan payment will be more than a 3rd of users rent and throws warning 
+        {
+
+            if (Expense.expenses[8]>(0.33*Expense.expenses[0]))                                         // checks if position [8]payment is greater that a 3rd of position [0] monthly income
+            {
+
+                Console.WriteLine("WARNING, YOUR HOME LOAN MONTHLY PAYMENT EXCEEDS A 3RD OF YOUR TOTAL MONTHLY INCOME THEREFORE YOU ARE UNLIKELY TO BE APPROVED FOR A LOAN"); // warning
+               
+            }
+
+        }
+        static void totalExpenses()                                 // method calculation total expenses by adding each from the array postion related to the expense
+        {
+           double totalExpense= Expense.expenses[1] + Expense.expenses[2]+ Expense.expenses[3] + Expense.expenses[4] + Expense.expenses[5] + Expense.expenses[6]+ Expense.expenses[7];
+           Console.WriteLine("Your total expenses are : R"+totalExpense);
+          
+                }
+        static void moneyLeft()                                         // method for calculating what is left after all expenses are deducted ( homeloan/rent included)
+        {
+            double totalExpense = Expense.expenses[1] + Expense.expenses[2] + Expense.expenses[3] + Expense.expenses[4] + Expense.expenses[5] + Expense.expenses[6] + Expense.expenses[7];
+
+            double residual = (Expense.expenses[0]-totalExpense);                                               // income [0] minus total expenses [1]-[9]
+            Console.WriteLine("Your total remaining value is :R"+residual);                                 
+
+        }
     }
 }
